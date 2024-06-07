e89b783852e9a67966273edcf57dbccc1d7154cf
Shaluwa357 <saluwabintzubayr@gmail.com>
2024-06-07 05:27:28 -0700

    Add files via upload
    QUESTION ONE

---
+++ LeapMonth.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.leapmonth;
+
+/**
+ *
+ * @author bosslady
+ */
+public class LeapMonth {
+
+    public static void main(String[] args) {
+        int year = 2023;
+        
+        boolean isLeapMonth = (year % 4 == 0);
+        isLeapMonth = isLeapMonth &&(year % 100 !=0);
+        isLeapMonth = isLeapMonth ||(year % 400 ==0);
+        if (isLeapMonth){
+            System.out.println("The given year has a leap month");
+        } else {
+        System.out.println("The given year does not have a leap month");
+    }
+ }
+}
